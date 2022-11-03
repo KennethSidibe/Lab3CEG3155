952c98944241e1af2c71384788ca3d49cee5fe04
Sidibe <Elstevehcks@gmail.com>
2022-11-02 21:01:52 -0400

    Add base entity for T flip flop

---
+++ tFlipFlop.vhd
@@ -0,0 +1,19 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+entity tFlipFlop is
+	port(
+	
+		i_t : in std_logic;
+		i_clock : in std_logic;
+		o_q, o_qBar : out std_logic
+
+	);
+end entity tFlipFlop;
+
+architecture rtl of tFlipFlop is
+
+    begin 
+
+
+end rtl; 
